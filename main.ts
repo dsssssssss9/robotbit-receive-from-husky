4a78195c4d1494230bc7530d977765f8ec4f090d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-21 22:01:25 +0000

    test motor & servo

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
 input.onButtonPressed(Button.A, function () {
+    AllFwd()
+})
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(robotbit.Servos.S1, 90)
+    basic.pause(1000)
+    robotbit.Servo(robotbit.Servos.S1, 180)
+    basic.pause(2000)
+    robotbit.Servo(robotbit.Servos.S1, 0)
+})
+function AllFwd () {
     robotbit.MotorRun(robotbit.Motors.M1A, 100)
     robotbit.MotorRun(robotbit.Motors.M2A, 100)
-    basic.pause(1000)
+    basic.pause(2000)
     robotbit.MotorStopAll()
-})
+}
 basic.showIcon(IconNames.Yes)
 let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.setBrightness(8)
